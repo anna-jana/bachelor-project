5d34aec64eb207863a51e3f0276c119011670ac1
Janik Riess <j.riess@wtnet.de>
2019-04-05 17:28:04 +0200

    approx for the density

--- axion_relic_density.py
+++ axion_relic_density.py
@@ -32,14 +32,14 @@ n = (3*p - 1) / 2 # nth bessel function
 # t = A p / m_a
 # a_osc / a_i = (t / ti) ^ p = (A p / m_a ti) ^ p
 a0 = 1
-A = 1
+A = 2
 a_osc_prime = 30
 # only valid for a ~ t^p
 ma_times_ti = A * p / a_osc_prime**(1/p)
 t_init = ma_times_ti / m_a
 a_osc = a_osc_prime * a0
 # t_osc = A * p / m_a
-# a_osc = a0 * (t_osc / t_init) ** p
+# a_osc2 = a0 * (t_osc / t_init) ** p
 a_end = a0 * 1e3
 # t_init = 1.0 # inital time [1 / (1e-20 eV)]
 t_end = t_init * (a_end / a0)**(1/p)
@@ -78,12 +78,19 @@ w_a = P_a / rho_a # eq. of state
 rho_c = 3 * H**2 / (8*pi*G) # critical density
 density_parameter = rho_a / rho_c
 
+# approximate the density when the axion field is oscillating
+osc_index = np.where(a >= a_osc)[0][0] # find the index where a > a_osc for the first time
+rho_a_osc = rho_a[osc_index]
+valid_a = a[osc_index:]
+rho_a_approx = rho_a_osc * (a_osc / valid_a)**3
+
 # plot all the stuff
 # field
 plt.subplot(2, 2, 1)
 plt.semilogx(a_prime, phi)
 plt.ylabel(r"Axion Field $\phi$")
 # TODO: plot the a_osc position using a vertical dashed line
+plt.axvline(a_osc, linestyle="--", color="black")
 
 # mass vs hubble
 plt.subplot(2, 2, 2)
@@ -91,17 +98,22 @@ plt.loglog(a_prime, H, label="Hubble")
 # plt.loglog(a_prime, p / t, label="Hubble formula")
 plt.loglog(a_prime, np.ones(a_prime.size) * m_a / 2, label=r"$m_a / 2$")
 plt.legend()
+plt.axvline(a_osc, linestyle="--", color="black")
 
 # eos
 plt.subplot(2, 2, 3)
 plt.semilogx(a_prime, w_a)
 plt.ylabel("Equation of State $w$")
 plt.xlabel("Scale Factor $a / a_i$")
+plt.axvline(a_osc, linestyle="--", color="black")
 
 # density
 plt.subplot(2, 2, 4)
 plt.loglog(a_prime, rho_a, label="Exact Density")
+plt.loglog(valid_a / a0, rho_a_approx, label="Approx. Density")
+plt.xlabel("Scale Factor $a / a_i$")
 plt.legend()
+plt.axvline(a_osc, linestyle="--", color="black")
 
 plt.tight_layout()
 plt.show()
