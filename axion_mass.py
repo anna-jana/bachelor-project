a44f1b5811ee7e344d306d7bde0f378e525e03b6
Janik Riess <j.riess@wtnet.de>
2019-05-16 14:14:21 +0200

    calibration for fox

--- axion_mass.py
+++ axion_mass.py
@@ -15,86 +15,6 @@ from scipy.interpolate import PchipInterpolator
 import config
 model = config.Model()
 
-################################## m_a at T = 0 ####################################
-###### marsh review ######
-def m_a_at_abs_zero_from_marsh(f_a):
-    return 6e-10 * 1e16 * 1e9 / f_a
-
-###### shellard ######
-# correct
-def m_a_at_abs_zero_from_shellard(f_a):
-    prefactor = model.m_pi0 * model.f_pi0 * np.sqrt(model.m_u * model.m_d) / (model.m_u + model.m_d)
-    # print(prefactor)
-    return prefactor / f_a
-
-############################## m_a at low T < Lambda_QCD ##########################
-###### shellard #######
-Lambda_shellard = 400e6
-
-# checked
-def m_a_at_low_T_from_shellard(T, f_a):
-    m_a = np.sqrt(
-            1.46e-3 * Lambda_shellard**4 *
-            (1 + 0.5*T/Lambda_shellard) /
-            (1 + (3.53*T/Lambda_shellard)**7.48)
-        ) / f_a
-    return m_a
-
-############################ m_a at high T > Lambda_QCD #########################
-######## fox et al. ########
-def m_a_at_high_T_from_fox(T, f_a, with_correction):
-    Lambda = model.Lambda_QCD
-    # Lambda = Lambda_shellard
-    C = 0.018
-    n_fox = 4
-    d = 1.2
-    alpha = 0.5
-    # m_a = np.sqrt(2) * C * m_a_at_abs_zero_from_marsh(f_a) * (Lambda / T)**n_fox
-    m_a = m_a_at_abs_zero_from_shellard(f_a) * C * (Lambda / 200e6)**alpha * (Lambda / T)**n_fox
-    if with_correction:
-        correction_factor = (1 - np.log(Lambda / T))**d
-        m_a *= correction_factor
-    return m_a
-
-######## shellard et al. ##########
-# approximation to full DGA result
-n_shellard = 6.68
-alpha_a = 1.68e-7
-
-def m_a_at_high_T_from_shellard(T, f_a):
-    m_a = np.sqrt(alpha_a) * Lambda_shellard**2 / (f_a * (T / Lambda_shellard)**(n_shellard/2))
-    return m_a
-
-# full IILA result
-T3 = 0.45e9
-T4 = 1.2e9
-T5 = 4.2e9
-T6 = 100e9
-
-d = np.array([[-15.6, -6.68, -0.947, +0.555],
-              # [+15.4, -7.04, -0.139, np.NAN], # data in paper is wrong
-              [-15.4, -7.04, -0.139, np.NAN],
-              [-14.8, -7.47, -0.0757, np.NAN]])
-
-max_deg = np.array([3, 2, 2], dtype=np.int)
-
-def m_a_full_IILA_shellard(T, f_a):
-    log_T_over_Lambda = np.log(T / Lambda_shellard)
-    def calc_exponent(N_f):
-        return sum(d[N_f - 3, n]*log_T_over_Lambda**n for n in range(0, max_deg[N_f - 3] + 1))
-    # exponent = np.where((T > T3) & (T < T4), calc_exponent(3), np.where((T > T4) & (T < T5), calc_exponent(4), calc_exponent(5)))
-    exponent = \
-        np.where(T < T3,
-            np.NAN,
-            np.where(T < T4,
-                calc_exponent(3),
-                np.where(T < T5,
-                    calc_exponent(4),
-                    np.where(T < T6,
-                        calc_exponent(5),
-                        np.NAN))))
-    return np.sqrt(Lambda_shellard**4 * np.exp(exponent)) / f_a
-
 ######################## numerical function from Borsamyi et al. ########################
 # data points for $chi_\mathrm{top}$ from Borsamyi et al. S10.3
 # T[MeV], - log10( \chi / fm^-4 )
@@ -140,4 +60,91 @@ chi_interp_plot = PchipInterpolator(T_plot, chi_plot)
 
 def m_a_from_chi(T, f_a):
     return np.sqrt(chi_interp_paper(T)) / f_a
+
+################################## m_a at T = 0 ####################################
+###### marsh review ######
+def m_a_at_abs_zero_from_marsh(f_a):
+    return 6e-10 * 1e16 * 1e9 / f_a
+
+###### shellard ######
+# correct
+def m_a_at_abs_zero_from_shellard(f_a):
+    prefactor = model.m_pi0 * model.f_pi0 * np.sqrt(model.m_u * model.m_d) / (model.m_u + model.m_d)
+    # print(prefactor)
+    return prefactor / f_a
+
+############################## m_a at low T < Lambda_QCD ##########################
+###### shellard #######
+Lambda_shellard = 400e6
+
+# checked
+def m_a_at_low_T_from_shellard(T, f_a):
+    m_a = np.sqrt(
+            1.46e-3 * Lambda_shellard**4 *
+            (1 + 0.5*T/Lambda_shellard) /
+            (1 + (3.53*T/Lambda_shellard)**7.48)
+        ) / f_a
+    return m_a
+
+############################ m_a at high T > Lambda_QCD #########################
+######## fox et al. ########
+callibration_factor = None
+
+def m_a_at_high_T_from_fox(T, f_a, with_correction):
+    global callibration_factor
+    if callibration_factor is None:
+        callibration_factor = 1
+    Lambda = model.Lambda_QCD
+    C = 0.018
+    C *= callibration_factor
+    n_fox = 4
+    d = 1.2
+    alpha = 0.5
+    m_a = m_a_at_abs_zero_from_shellard(f_a) * C * (Lambda / 200e6)**alpha * (Lambda / T)**n_fox
+    if with_correction:
+        correction_factor = (1 - np.log(Lambda / T))**d
+        m_a *= correction_factor
+    return m_a
+
+Lambda_callibration = 100e6
+callibration_factor = m_a_from_chi(Lambda_callibration, 1.0) / m_a_at_high_T_from_fox(Lambda_callibration, 1.0, with_correction=False)
+
+######## shellard et al. ##########
+# approximation to full DGA result
+n_shellard = 6.68
+alpha_a = 1.68e-7
+
+def m_a_at_high_T_from_shellard(T, f_a):
+    m_a = np.sqrt(alpha_a) * Lambda_shellard**2 / (f_a * (T / Lambda_shellard)**(n_shellard/2))
+    return m_a
+
+# full IILA result
+T3 = 0.45e9
+T4 = 1.2e9
+T5 = 4.2e9
+T6 = 100e9
+
+d = np.array([[-15.6, -6.68, -0.947, +0.555],
+              # [+15.4, -7.04, -0.139, np.NAN], # data in paper is wrong
+              [-15.4, -7.04, -0.139, np.NAN],
+              [-14.8, -7.47, -0.0757, np.NAN]])
+
+max_deg = np.array([3, 2, 2], dtype=np.int)
+
+def m_a_full_IILA_shellard(T, f_a):
+    log_T_over_Lambda = np.log(T / Lambda_shellard)
+    def calc_exponent(N_f):
+        return sum(d[N_f - 3, n]*log_T_over_Lambda**n for n in range(0, max_deg[N_f - 3] + 1))
+    # exponent = np.where((T > T3) & (T < T4), calc_exponent(3), np.where((T > T4) & (T < T5), calc_exponent(4), calc_exponent(5)))
+    exponent = \
+        np.where(T < T3,
+            np.NAN,
+            np.where(T < T4,
+                calc_exponent(3),
+                np.where(T < T5,
+                    calc_exponent(4),
+                    np.where(T < T6,
+                        calc_exponent(5),
+                        np.NAN))))
+    return np.sqrt(Lambda_shellard**4 * np.exp(exponent)) / f_a
 
