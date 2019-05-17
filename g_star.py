aa8dd3817a2f7a13abda136c99d7b9546268e675
Janik Riess <j.riess@wtnet.de>
2019-05-17 17:27:55 +0200

    improved g_star

--- g_star.py
+++ g_star.py
@@ -4,9 +4,16 @@ It is given as data points as well as a fitting interpolation (Pchip)
 The temperature is given in eV.
 """
 
+from collections import namedtuple
+
 import numpy as np
 from scipy.interpolate import PchipInterpolator
 
+MeV = 1e6 # [eV]
+
+GStarModel = namedtuple("GStarModel", ["g_rho", "g_s", "g_rho_diff", "g_s_diff"])
+
+############################# g data from the table in the paper Borsamyi #######################
 # data from lattice paper (S4.3) from Borsamyi et al.
 # columns: [log10(T/MeV), g_rho, g_rho / g_s]
 data = np.array([
@@ -28,34 +35,61 @@ data = np.array([
     [5.45, 104.98, 1.00023],
 ])
 
-# g_s_data = np.loadtxt("g_s_data.dat")
-# g_s_over_g_rho_data = np.loadtxt("g_rho_over_g_s.dat")
-#
-# T_g_s = g_s_data[:, 0] * 1e6 # [eV]
-# g_s = g_s_data[:, 1]
-#
-# T_g_rho_over_g_s = g_s_over_g_rho_data[:, 0] * 1e6
-# T_g_rho = T_g_rho_over_g_s
-# g_rho_over_g_s = g_s_over_g_rho_data[:, 1]
-# g_rho = g_rho_over_g_s * g_s_interp(T_g_rho_over_g_s)
-# log10_T_per_MeV = data[:, 0]
-#
-# g_rho_from_plot = g_rho
-# g_s_from_plot = g_s
-
+# convert data from table in paper
+log10_T_per_MeV = data[:, 0]
 g_rho = data[:, 1]
 g_rho_per_s = data[:, 2]
-
-MeV = 1e6 # [eV]
 T = MeV * 10 ** log10_T_per_MeV # [eV]
 g_s = g_rho / g_rho_per_s # [1]
-
-# T = np.concatenate([T, T_g_rho])
-# sort_perm = np.argsort(T)
-# T =
-
+# interpolate
 g_rho_interp = PchipInterpolator(T, g_rho)
 g_s_interp = PchipInterpolator(T, g_s)
-
 g_rho_interp_diff = g_rho_interp.derivative()
 g_s_interp_diff = g_s_interp.derivative()
+borsamyi_paper_table = GStarModel(g_rho=g_rho_interp, g_s=g_s_interp, g_rho_diff=g_rho_interp_diff, g_s_diff=g_s_interp_diff)
+g_s_paper = g_s
+g_rho_paper = g_rho
+
+####################### g data extracted from plots + table from paper ###############
+# load extracted data from files
+g_s_data = np.loadtxt("g_s_data.dat")
+g_s_over_g_rho_data = np.loadtxt("g_rho_over_g_s.dat")
+
+# convert g_s data and combine with table
+T_g_s = np.concatenate([g_s_data[:, 0] * MeV, T])
+g_s = np.concatenate([g_s_data[:, 1], g_s])
+sort_perm = np.argsort(T_g_s)
+T_g_s = T_g_s[sort_perm]
+g_s = g_s[sort_perm]
+
+# remove bad indicies (found by hand)
+g_s_bad_indicies = [1,2,3,4,6,7,8,11,13,15,20]
+g_s = np.delete(g_s, g_s_bad_indicies)
+T_g_s = np.delete(T_g_s, g_s_bad_indicies)
+
+# interpolate g_s
+g_s_interp = PchipInterpolator(T_g_s, g_s)
+g_s_interp_diff = g_s_interp.derivative()
+
+# convert ratio data
+T_g_rho_over_g_s = g_s_over_g_rho_data[:, 0] * MeV
+g_rho_over_g_s = g_s_over_g_rho_data[:, 1]
+
+# comptute g_rho
+T_g_rho = np.concatenate([T_g_rho_over_g_s, T])
+g_rho = np.concatenate([g_rho_over_g_s * g_s_interp(T_g_rho_over_g_s), g_rho])
+sort_perm = np.argsort(T_g_rho)
+T_g_rho = T_g_rho[sort_perm]
+g_rho = g_rho[sort_perm]
+
+# remove bad indicies (found by hand)
+g_rho_bad_indicies = [1,2,3,4,5,7,8,9,10]
+g_rho = np.delete(g_rho, g_rho_bad_indicies)
+T_g_rho = np.delete(T_g_rho, g_rho_bad_indicies)
+
+# interpolate g_rho
+g_rho_interp = PchipInterpolator(T_g_rho, g_rho)
+g_rho_interp_diff = g_rho_interp.derivative()
+
+# make model
+borsamyi_table = GStarModel(g_rho=g_rho_interp, g_s=g_s_interp, g_rho_diff=g_rho_interp_diff, g_s_diff=g_s_interp_diff)
