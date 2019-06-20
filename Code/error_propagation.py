ac2bf164cd7dc85c419be078fd7dc9c00fb8e295
Janik Riess <j.riess@wtnet.de>
2019-06-20 16:17:25 +0200

    error_propagation

--- Code/error_propagation.py
+++ Code/error_propagation.py
@@ -3,6 +3,7 @@ import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.constants as c
 
 import config
 import eom
@@ -10,8 +11,11 @@ import axion_mass
 import g_star
 import potential
 
-theta_i = 1
-f_a = 1e12 * 1e9
+num_samples = int(sys.argv[1])
+num_threads = int(sys.argv[2])
+theta_i = float(sys.argv[3])
+f_a = float(sys.argv[4]) * 1e9 # input in GeV
+filename = sys.argv[5]
 
 model = eom.Model(axion_mass.m_a_from_chi_general, g_star.matched, potential.cosine, None)
 M_pl_err = 0.000014e19 * 1e9 / np.sqrt(8*np.pi)
@@ -22,6 +26,7 @@ m_u_err = (0.49 + 0.26) / 2 * 1e6
 m_d_err = (0.48 + 0.17) / 2 * 1e6
 m_pi0_err = 0.0005 * 1e6
 f_pi0_err = 5e6
+T0_err = 6e-5 * c.Boltzmann / c.elementary_charge
 
 # use the fllowing directly: h T0 rho_c
 
@@ -35,15 +40,14 @@ def sample(i):
     params.m_d = np.random.normal(loc=params.m_d, scale=m_d_err)
     params.m_pi0 = np.random.normal(loc=params.m_pi0, scale=m_pi0_err)
     params.f_pi0 = np.random.normal(loc=params.f_pi0, scale=f_pi0_err)
+    params.T0 = np.random.normal(loc=params.T0, scale=T0_err)
 
     model.parameter = params
     solver = model.get_solver(theta_i, f_a)
     return solver.compute_density_parameter()
 
-if __name__ == "__main__":
-    num_samples = int(sys.argv[1])
-    num_threads = int(sys.argv[2])
-    with mp.Pool(num_threads) as poolparty:
-        samples = poolparty.map(sample, range(num_samples))
-    np.savetxt("samples.txt", samples)
+with mp.Pool(num_threads) as poolparty:
+    samples = poolparty.map(sample, range(num_samples))
+
+np.savetxt(filename, samples)
 
